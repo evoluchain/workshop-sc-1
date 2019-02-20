68887b7221f66edcbea5c5d20e8899dbc060362e
Fabien Champigny <fabien@champigny.name>
2019-02-20 10:54:23 +0100

    Update packages dependencies versions

--- truffle-config.js
+++ truffle-config.js
@@ -4,6 +4,11 @@ require('babel-polyfill');
 
 module.exports = {
   networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*', // eslint-disable-line camelcase
+    },
     ganache: {
       host: 'localhost',
       port: 8545,
